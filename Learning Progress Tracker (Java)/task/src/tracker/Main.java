60a3efc08f0ae69a63e838c7c78eed119487a40f
Rinze-CodeGorilla <132885446+Rinze-CodeGorilla@users.noreply.github.com>
2023-08-07 15:36:46 +0200

    Stage 1

--- Learning Progress Tracker (Java)/task/src/tracker/Main.java
+++ Learning Progress Tracker (Java)/task/src/tracker/Main.java
@@ -1,7 +1,20 @@
 package tracker;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("Learning Progress Tracker");
+        var isRunning = true;
+        var scanner = new Scanner(System.in);
+        while (isRunning) {
+            var command = scanner.nextLine();
+            switch(command.trim()) {
+                case "exit" -> isRunning = false;
+                case "" -> System.out.println("No input.");
+                default -> System.out.println("Unknown command!");
+            }
+        }
+        System.out.println("Bye!");
     }
 }
